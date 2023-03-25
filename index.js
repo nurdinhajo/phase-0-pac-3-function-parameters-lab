082fbf506b1ee96f7db4164ee079de7bc380218f
nuru <nurudiin222@gmail.com>
2023-03-25 16:52:32 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,16 @@
+function introduction(name){
+ return `Hi, my name is ${name}.`
+}
+
+function introductionWithLanguage(name, language ){
+ return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
+
+function introductionWithLanguageOptional(name){
+ return `Hi, my name is ${name} and I am learning to program in JavaScript.`
+}
+
+
+function introductionWithLanguageOptional(name, language = "JavaScript"){
+ return `Hi, my name is Gracie and I am learning to program in ${language}.`
+}
